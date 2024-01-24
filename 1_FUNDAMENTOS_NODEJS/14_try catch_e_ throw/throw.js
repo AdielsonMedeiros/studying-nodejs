fba7d41b661fa8cfbc4cdf8afcca96de2374cce1
Adielson Medeiros <medeirosadielson@gmail.com>
2024-01-24 11:48:08 -0300

    TryCatchThrow

---
+++ 1_FUNDAMENTOS_NODEJS/14_try catch_e_ throw/throw.js
@@ -0,0 +1,12 @@
+
+const x = 10
+
+
+//checar se x é um numero inteiro
+
+if(!Number.isInteger(x)){
+    throw new Error("o valor de x não é um número de inteiro!")
+
+}
+
+console.log('continuando o codigo...')
