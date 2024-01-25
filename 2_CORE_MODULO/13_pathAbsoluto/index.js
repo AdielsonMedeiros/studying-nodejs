422f68a6d775919fb85eef432527eea70e44331c
Adielson Medeiros <medeirosadielson@gmail.com>
2024-01-25 16:42:51 -0300

    path e pathabsoluto

---
+++ 2_CORE_MODULO/13_pathAbsoluto/index.js
@@ -0,0 +1,18 @@
+
+
+const path = require('path')
+
+
+// path absoluto
+console.log(path.resolve('test.txt'))
+
+
+//formar path
+
+const midFolder = "relatorios"
+const fileName = "adielson.txt"
+
+const finalPath = path.join('/', 'arquivos', midFolder, fileName)
+
+console.log(finalPath)
+
