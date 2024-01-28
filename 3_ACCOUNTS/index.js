6650a2dfec0e05e504fc2a9b033a2e61671350e6
Adielson Medeiros <medeirosadielson@gmail.com>
2024-01-28 12:38:38 -0300

    criar conta feito

--- 3_ACCOUNTS/index.js
+++ 3_ACCOUNTS/index.js
@@ -3,6 +3,7 @@
 
 import chalk from 'chalk'
 import fs from 'fs'
+import inquirer from 'inquirer'
 
 
 // modulos internos
@@ -27,9 +28,47 @@ async function operation(){
 .catch((err)=> console.log(err))
 }
 
-// criar uma conta 
+// criar uma conta
 
 function createAccount(){
     console.log(chalk.bgGreen.black('Parabéns por escolher esse banco ruim!'))
     console.log(chalk.green('Defina as opções da sua conta zé mané'))
+
+    buildAccount()
+}
+
+function buildAccount(){
+    
+    inquirer.prompt([
+        {
+            name:'accountName',
+            message: 'Digite um nome para a sua conta: ',
+        },
+    ])
+    .then((answer) => {
+
+        const accountName = answer['accountName']
+        console.info(accountName)
+
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome! '))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(
+            `accounts/${accountName}.json`,
+            '{"balance": 0}',
+            function (err){
+                console.log(err)
+            }
+        )
+        console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+        operation()
+    })
+    .catch((err)=> console.log(err))
 }
